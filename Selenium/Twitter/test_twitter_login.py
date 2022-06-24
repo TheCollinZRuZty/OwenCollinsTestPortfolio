5638502072789e40d7475b671ca1fb0f6754be1b
Owen Collins <owencollins@DESKTOP-RL02N8E.lan>
2022-06-24 11:31:53 +0100

    remove pass

--- Selenium/Twitter/test_twitter_login.py
+++ Selenium/Twitter/test_twitter_login.py
@@ -1,4 +1,4 @@
-import os, sys, unittest
+import os, sys, unittest, time
 from webbrowser import BaseBrowser
 from selenium import webdriver 
 import config as cfg
@@ -18,7 +18,6 @@ class test_twitter_login(unittest.TestCase):
         chrome_options.add_argument("--diasble-dev-shm-usage")
         chrome_options.add_argument("window-size=1920,1080")
         browser = webdriver.Chrome(chrome_options=chrome_options)
-        browser.set_window_position(0,0)
         browser.get(cfg.url)
     
     @classmethod
@@ -28,86 +27,72 @@ class test_twitter_login(unittest.TestCase):
     def tearDown(self):
         browser.switch_to.default_content()
 
-    def test_twitter_login_001(self):
-        """Verify Twitter logo appears as expected. 
-        """
-        wait=WebDriverWait(browser,30)
-        wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/svg/g/path')))
-        self.assertTrue(browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/svg/g/path'),'The Twitter logo does not appear as expected.')
+    # def test_twitter_login_001(self):
+    #     """Verify Twitter logo appears as expected. 
+    #     """
+    #     wait=WebDriverWait(browser,30)
+    #     wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/svg/g/path')))
+    #     self.assertTrue(browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/svg/g/path'),'The Twitter logo does not appear as expected.')
         
     def test_twitter_login_002(self):
         """Verify "Happening now" Label appears.
         """
         wait=WebDriverWait(browser,30)
-        wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[1]/span')))
-        act_text=browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[1]/span').text
-        expected_text='Happening now'
-        self.assertTrue(act_text==expected_text,'The "Happening now" Label does not appear as expected.')
+        wait.until(EC.presence_of_element_located((By.XPATH,"//span[contains(text(),'Happening now')]")))
+        self.assertTrue(browser.find_element_by_xpath("//span[contains(text(),'Happening now')]"),'The "Happening now" Label does not appear as expected.')
 
     def test_twitter_login_003(self):
         """Verify "Join Twitter today." Label appears.
         """
         wait=WebDriverWait(browser,30)
-        wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[2]/span')))
-        act_text=browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[2]/span').text
-        expected_text='Join Twitter today.'
-        self.assertTrue(act_text==expected_text,'The "Join Twitter today." Label does not appear as expected.')
+        wait.until(EC.presence_of_element_located((By.XPATH,"//span[contains(text(),'Join Twitter today.')]")))
+        self.assertTrue(browser.find_element_by_xpath("//span[contains(text(),'Join Twitter today.')]"),'The "Join Twitter today." Label does not appear as expected.')
 
     def test_twitter_login_004(self):
         """Verify "Sign up with Google" Button appears. 
         """
         wait=WebDriverWait(browser,30)
-        wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="container"]/div/div[2]/span[1]')))
-        text=browser.find_element_by_xpath('//*[@id="container"]/div/div[2]/span[1]').text
-        expected_text='Sign up with Google'
-        self.assertTrue(browser.find_element_by_xpath(text==expected_text),'The "Sign up with Google" Button does not appear as expected.')
-        self.assertTrue(browser.find_element_by_xpath('//*[@id="container"]/div/div[2]/span[1]'),'The "Sign up with Google" Button does not appear as expected.')
+        wait.until(EC.presence_of_element_located((By.XPATH,"//span[contains(text(),'Sign up with Google')]")))
+        self.assertTrue(browser.find_element_by_xpath("//span[contains(text(),'Sign up with Google')]"),'The "Sign up with Google" Button does not appear as expected.')
     
     def test_twitter_login_005(self):
         """Verify "Sign up with Apple" Button appears.
         """
         wait=WebDriverWait(browser,30)
-        wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[2]/div/span/span')))
-        text=browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[2]/div/span/span').text
-        expected_text='Sign up with Apple'
-        self.assertTrue(browser.find_element_by_xpath(text==expected_text),'The "Sign up with Apple" Button does not appear as expected.')
-        self.assertTrue(browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[2]/div/span/span'),'The "Sign up with Apple" Button does not appear as expected.')
+        wait.until(EC.presence_of_element_located((By.XPATH,"//span[contains(text(),'Sign up with Apple')]")))
+        self.assertTrue(browser.find_element_by_xpath("//span[contains(text(),'Sign up with Apple')]"),'The "Sign up with Apple" Button does not appear as expected.')
 
     def test_twitter_login_006(self):
         """Verify "Sign up with phone or email" Button appears..
         """
         wait=WebDriverWait(browser,30)
-        wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/a/div/span/span')))
-        text=browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/a/div/span/span').text
-        expected_text='Sign up with phone or email'
-        self.assertTrue(browser.find_element_by_xpath(text==expected_text),'The "Sign up with Apple" Button does not appear as expected.')
-        self.assertTrue(browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/a/div/span/span'),'The  "Sign up with phone or email" Button does not appear as expected.')
+        wait.until(EC.presence_of_element_located((By.XPATH,"//span[contains(text(),'Sign up with phone or email')]")))
+        self.assertTrue(browser.find_element_by_xpath("//span[contains(text(),'Sign up with phone or email')]"),'The  "Sign up with phone or email" Button does not appear as expected.')
 
     def test_twitter_login_007(self):
         """Verify "Already have an account" string appears.
         """
         wait=WebDriverWait(browser,30)
-        wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/div/span')))
-        text=browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/div/span').text
-        expected_text='Already have an account?'
-        self.assertTrue(browser.find_element_by_xpath(text==expected_text),'The "Sign up with Apple" Button does not appear as expected.')
+        wait.until(EC.presence_of_element_located((By.XPATH,"//span[contains(text(),'Already have an account?')]")))
+        self.assertTrue(browser.find_element_by_xpath(browser.find_element_by_xpath("//span[contains(text(),'Already have an account?')]")),'The "Sign up with Apple" Button does not appear as expected.')
     
     def test_twitter_login_008(self):
         """Verify "Sign in" Button appears.
         """
         wait=WebDriverWait(browser,30)
-        wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a/div')))
-        text=browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a/div').text
-        expected_text='Already have an account?'
-        self.assertTrue(browser.find_element_by_xpath(text==expected_text),'The "Sign up with Apple" Button does not appear as expected.')
-        self.assertTrue(browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a/div'),'The "Sign in" Button does not appear as expected.')
+        wait.until(EC.presence_of_element_located((By.XPATH,"//span[contains(text(),'Sign in')]")))
+        self.assertTrue(browser.find_element_by_xpath("//span[contains(text(),'Sign in')]"),'The "Sign in" Button does not appear as expected.')
 
     def test_twitter_login_009(self):
-        """Verify "Sign up with Google" Accepts login.
+        """Verify "Sign up with Sign In" Accepts login.
         """
         wait=WebDriverWait(browser,30)
-        wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a/div')))
-        browser.find_element_by_xpath('//*[@id="container"]/div/div[2]/span[1]').click()
-        expected_text='Already have an account?'
-        self.assertTrue(browser.find_element_by_xpath(text==expected_text),'The "Sign up with Apple" Button does not appear as expected.')
+        wait.until(EC.presence_of_element_located((By.XPATH,"//span[contains(text(),'Sign in')]")))
+        browser.find_element_by_xpath("//span[contains(text(),'Sign in')]").click()
+        wait.until(EC.presence_of_element_located({By.XPATH,"//span[contains(text(),'Sign in to Twitter')]"}))
+        browser.find_element_by_xpath('//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input').send_keys(cfg.twitter_user_name)
+        wait.until(EC.element_to_be_clickable((By.XPATH,"//body/div[@id='react-root']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[6]/div[1]")))
+        browser.find_element_by_xpath("//body/div[@id='react-root']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[6]/div[1]").click()
+        wait.until(EC.presence_of_element_located((By.XPATH,"//body/div[@id='react-root']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[6]/div[1]")))
+        browser.find_element_by_xpath("//body/div[@id='react-root']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/label[1]/div[1]/div[2]/div[1]/input[1]").send_keys(cfg.twitter_password)
         self.assertTrue(browser.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a/div'),'The "Sign in" Button does not appear as expected.')
